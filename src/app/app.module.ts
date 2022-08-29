4a4fc64072a83e5d6204ea3915541528c2b3e3f4
Athulya <athulyabalakrishnanv@gmail.com>
2022-08-29 12:13:21 +0530

    Cartpage Design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { SearchComponent } from './search/search.component';
 import { TagsComponent } from './tags/tags.component';
 import { CartPageComponent } from './cart-page/cart-page.component';
 import { FoodpageComponent } from './foodpage/foodpage.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,6 +20,7 @@ import { FoodpageComponent } from './foodpage/foodpage.component';
     TagsComponent,
     CartPageComponent,
     FoodpageComponent,
+    NotFoundComponent,
     
   ],
   imports: [
--- src/app/cart-page/cart-page.component.ts
+++ src/app/cart-page/cart-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../services/cart.service';
+import { FoodService } from '../services/food/food.service';
+import { Cart } from '../shared/models/Cart';
+import { CartItem } from '../shared/models/CartItem';
 
 @Component({
   selector: 'app-cart-page',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart-page.component.css']
 })
 export class CartPageComponent implements OnInit {
+  cart!: Cart;
+  constructor(private cartService: CartService) { 
+  // , private foodService: FoodService
+    // let foods = foodService.getAll();
+    // cartService.addToCart(foods[1]);
+    // cartService.addToCart(foods[3]);
+    // cartService.addToCart(foods[5]);
 
-  constructor() { }
+    this.setCart();
+  }
 
   ngOnInit(): void {
+  }
+  setCart() {
+    this.cart = this.cartService.getCart();
+  }
+  removeFromCart(cartItem: CartItem) {
+    this.cartService.removeFromCart(cartItem.food.id);
+    this.setCart();
+  }
+  changeQuantity(cartItem:CartItem, quantityInString:string){
+    const quantity = parseInt(quantityInString);
+    this.cartService.changeQuantity(cartItem.food.id, quantity);
+    this.setCart();
   }
 
 }
